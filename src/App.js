5db6b1578360175be91929a824cc871bc0a8ae4b
kwkim <ruddns0208@gmail.com>
2020-08-31 12:53:41 +0900

    상태관리 예제

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropsComponent from "./ch03/PropsComponent";
+import BooleanComponent from "./ch03/BooleanComponent";
+import StateExample from "./ch03/StateExample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    /*    <div>
+            <PropsComponent name="두잇 리액트"/>
+            <div><b>지루할 떄:</b><BooleanComponent bored/></div>
+            <div><b>즐거울떄:</b><BooleanComponent/></div>
+        </div>*/
+        <div>
+            <StateExample/>
+        </div>
+
+    );
 }
 
 export default App;
